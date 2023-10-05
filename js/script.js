85df621ee51ebdc9abb5e9dc48e6974c5f6c6efb
Eugene <eugenetiron@yahoo.com>
2023-10-05 20:23:36 +0100

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 let menuBtn = document.querySelector('#menu-btn');
 let closeBtn = document.querySelector('#close-navbar');
 let navbar = document.querySelector('.navbar');
-
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.search-form')
 
 menuBtn.onclick = () => {
     navbar.classList.toggle('active')
 }
+
+searchBtn.onclick = () => {
+    searchForm.classList.toggle('active')
+}
+
+closeBtn.onclick = () => {  
+    navbar.classList.remove('active')
+};
+
+
+document.querySelector('html').onscroll = () => {
+    console.log("Scrolling")
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active')
+};
+
+
 
 // menuBtn.onclick = function() { 
 //     let computedStyle = window.getComputedStyle(navbar);
@@ -17,9 +35,3 @@ menuBtn.onclick = () => {
 //         navbar.style.left = '-110%';
 //     }
 // };
-
-closeBtn.onclick = function() {  
-    navbar.classList.remove('active')
-};
-
-22:07
